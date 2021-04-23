27bcda0c291478b61aa9ef970d24f022820dc341
Niels NTG <ntg999@gmail.com>
2021-04-23 14:07:30 +0200

    Remove redundant methods

--- interfaceUtils.py
+++ interfaceUtils.py
@@ -74,26 +74,6 @@ def fill(fromX, fromY, fromZ, toX, toY, toZ, material, fillMode="replace"):
     )
 
 
-# Create solid shape filling the given area. Non-air blocks are unaffected.
-def fillEmpty(fromX, fromY, fromZ, toX, toY, toZ, material):
-    return fill(fromX, fromY, fromZ, toX, toY, toZ, material, fillMode="keep")
-
-
-# Create hollow shape. Blocks inside the area are replaced with air.
-def hollowFill(fromX, fromY, fromZ, toX, toY, toZ, material):
-    return fill(fromX, fromY, fromZ, toX, toY, toZ, material, fillMode="hollow")
-
-
-# Create hollow shape. Blocks inside the area are unaffected.
-def outlineFill(fromX, fromY, fromZ, toX, toY, toZ, material):
-    return fill(fromX, fromY, fromZ, toX, toY, toZ, material, fillMode="outline")
-
-
-# Replace all blocks in the given area with air.
-def clear(fromX, fromY, fromZ, toX, toY, toZ):
-    return fill(fromX, fromY, fromZ, toX, toY, toZ, "air")
-
-
 # --------------------------------------------------------- block buffers
 
 blockBuffer = []
