f1385b556d714422bb45261ff0a9ab803bbb17d1
skylerf1 <31687631+skylerf1@users.noreply.github.com>
2021-05-18 13:23:05 +0200

    city network ready for use

--- street.py
+++ street.py
@@ -5,10 +5,8 @@ import nbt
 import numpy as np
 from random import sample
 
-intersection = Structure.Structure('intersection', -80, 3, 135, 1)
-#intersection.place()
-objects = ['tree1','tree2','tree3','tree4','lantern']
-crosswalks = ['crosswalk_s', 'crosswalk_plain']
+objects = ['street/tree1','street/tree2','street/tree3','street/tree4','street/lantern']
+crosswalks = ['street/crosswalk_s', 'street/crosswalk_plain']
 
 def vertical_street(x, y, z):
     x_increase = 0
@@ -17,7 +15,7 @@ def vertical_street(x, y, z):
     
     #sidewalk
     for i in range(8):
-        sidewalk = Structure.Structure('sidewalk_object_middle', x + x_increase, y, z, 0)
+        sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z, 0)
         x_increase += 8
         sidewalk.place()
 
@@ -26,7 +24,7 @@ def vertical_street(x, y, z):
     y_increase = -1   
     z_increase = 5
     for i in range(8):
-        asphalt = Structure.Structure('asphalt_plain', x + x_increase, y + y_increase, z + z_increase, 0)
+        asphalt = Structure.Structure('street/asphalt_plain', x + x_increase, y + y_increase, z + z_increase, 0)
 
         if i == 0 or i == 7:
             if np.random.randint(low=1, high=3) == 2:
@@ -43,10 +41,10 @@ def vertical_street(x, y, z):
     #sidewalk 2
     for i in range(8):
         if np.random.randint(low=1, high=3) == 2:
-            sidewalk = Structure.Structure('sidewalk_object_middle', x + x_increase, y, z + z_increase, 0)
+            sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z + z_increase, 0)
         
         else:
-            sidewalk = Structure.Structure('sidewalk_object_middle', x + x_increase, y, z + z_increase, 0)
+            sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z + z_increase, 0)
         
         x_increase += 8
         sidewalk.place()
@@ -76,12 +74,12 @@ def vertical_street(x, y, z):
 def intersection(x,y,z):
     epsilon = np.random.randint(low=1, high=4)
     if epsilon == 2:
-        intersection = Structure.Structure('intersection_plain', x, y -1, z, 0)
+        intersection = Structure.Structure('street/intersection_plain', x, y -1, z, 0)
         
     elif epsilon == 3:
-        intersection = Structure.Structure('intersection_1_stoplight', x, y-1, z, 0)
+        intersection = Structure.Structure('street/intersection_1_stoplight', x, y-1, z, 0)
     else: 
-        intersection = Structure.Structure('intersection_2_stoplight', x, y-1, z, 0)
+        intersection = Structure.Structure('street/intersection_2_stoplight', x, y-1, z, 0)
     
     intersection.place()
 
@@ -96,11 +94,11 @@ def intersection(x,y,z):
 
 def select_object(x, y, z, x_increase, y_increase, z_increase, intersect=False): 
     
-    lantern = Structure.Structure('lantern', x + x_increase, y, z + z_increase, 0)
-    tree1 = Structure.Structure('tree_1', x - 1 + x_increase, y, z - 1 + z_increase, 0)
-    tree2 = Structure.Structure('tree_2', x + x_increase, y, z + z_increase, 0)
-    tree3 = Structure.Structure('tree_3', x - 1 + x_increase, y, z - 1 + z_increase, 0)
-    tree4 = Structure.Structure('tree_4', x - 1 + x_increase, y, z - 1 + z_increase, 0)
+    lantern = Structure.Structure('street/lantern', x + x_increase, y, z + z_increase, 0)
+    tree1 = Structure.Structure('street/tree_1', x - 1 + x_increase, y, z - 1 + z_increase, 0)
+    tree2 = Structure.Structure('street/tree_2', x + x_increase, y, z + z_increase, 0)
+    tree3 = Structure.Structure('street/tree_3', x - 1 + x_increase, y, z - 1 + z_increase, 0)
+    tree4 = Structure.Structure('street/tree_4', x - 1 + x_increase, y, z - 1 + z_increase, 0)
     
 
     #usually trees, sometimes lanterns sometimes nothing
@@ -121,26 +119,128 @@ def select_object(x, y, z, x_increase, y_increase, z_increase, intersect=False):
         element = stoplight
     return element
     
-  
-#Vertical Lines
-#intersection(-330, 4, 460)
-#vertical_street(-330 + 22, 4, 460)
-#intersection(-330 + 86, 4, 460)
-#vertical_street(-330 + 108, 4, 460)
-#intersection(-330 + 172, 4, 460)
+def horizontal_street(x, y, z):
+    x_increase = 0
+    y_increase = 0
+    z_increase = 0
+    
+    #sidewalk
+    for i in range(8):
+        sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z + z_increase, 1)
+        z_increase += -8
+        sidewalk.place()
+
+    #asphalt
+    x_increase = -5
+    y_increase = -1   
+    z_increase = 0
+    for i in range(8):
+        asphalt = Structure.Structure('street/asphalt_plain', x + x_increase, y + y_increase, z + z_increase, 1)
+
+        if i == 0 or i == 7:
+            if np.random.randint(low=1, high=3) == 2:
+                type_crosswalk = sample(crosswalks, 1)[0]
+                crosswalk = Structure.Structure(type_crosswalk, x + x_increase, y + y_increase, z + z_increase, 1)
+                crosswalk.place()
+            else:
+                asphalt.place()
+        else:        
+            asphalt.place()
+        z_increase += -8
+    
+    #sidewalk 2
+    x_increase += -12
+    z_increase += 8   
+    for i in range(8):
+        if np.random.randint(low=1, high=3) == 2:
+            sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z + z_increase, 1)
+        
+        else:
+            sidewalk = Structure.Structure('street/sidewalk_object_middle', x + x_increase, y, z + z_increase, 1)
+        
+        z_increase += 8
+        sidewalk.place()
+
+    #objects sidewalk
+    x_increase = -3
+    y_increase = 0   
+    z_increase = 2
+    for i in range(8):
+        element = select_object(x, y, z,x_increase, y_increase, z_increase) 
+        if element != None:
+            element.place()
+
+        z_increase += -8 
+
+    #objects sidewalk2
+    x_increase += -17
+    y_increase = 0   
+    z_increase = 2
+
+    for i in range(8):
+        element = select_object(x, y, z, x_increase, y_increase, z_increase) 
+        if element != None:
+            element.place()
+
+        z_increase += -8
+#spawns the streets. 10 blocks from the outline of the 256x256 grid
+def city_network(x, y, z):
+    init_x = 31
+    init_z = -13
+    #horizontal_street(x + 95, y, z + init_z + 65)
+
+    horizontal_street(x + init_x + 64, y, z + init_z + 78)
+    horizontal_street(x + init_x + 150, y, z + init_z + 78)
+    horizontal_street(x + init_x + 64, y, z + init_z + 78 + 86)
+    horizontal_street(x + init_x + 150, y, z + init_z + 78 + 86)
+    horizontal_street(x + init_x + 64, y, z + init_z + 78 + 86 + 86)
+    horizontal_street(x + init_x + 150, y, z + init_z + 78 + 86 + 86)
+
+    intersection(x + init_x + 43, y, z + init_z + 86)
+    intersection(x + init_x + 43, y, z + init_z + 86 + 86)
+    intersection(x + init_x + 43 + 86, y, z + init_z + 86)
+    intersection(x + init_x + 43 + 86, y, z + init_z + 86 + 86)
+
+    vertical_street(x + init_x + 65 - 86, y, z + init_z + 86)
+    vertical_street(x + init_x + 65, y, z + init_z + 86)
+    vertical_street(x + init_x + 65 + 86, y, z + init_z + 86)
+    vertical_street(x + init_x + 65 - 86, y, z + init_z + 86 + 86)
+    vertical_street(x + init_x + 65, y, z + init_z + 86 + 86)
+    vertical_street(x + init_x + 65 + 86, y, z + init_z + 86 + 86)
 
 
-#intersection(-330, 4, 460 + 64)
-#vertical_street(-330 + 22, 4, 460 + 64)
-#intersection(-330 + 86, 4, 460 + 64)
-#vertical_street(-330 + 108, 4, 460+ 64)
-#intersection(-330 + 172, 4, 460+ 64)
+#square city 
 
-#intersection(-330, 4, 460+128)
-#vertical_street(-330 + 22, 4, 460+128)
-#intersection(-330 + 86, 4, 460+128)
-#vertical_street(-330 + 108, 4, 460+128)
-#intersection(-330 + 172, 4, 460+128)
+#left side city net
+#intersection(-1031, 4, 508)
+#horizontal_street(-1031 + 21, 4, 508 + 78)
+#vertical_street(-1031 + 22, 4, 508)
+#horizontal_street(-1031 + 107, 4, 508 + 78)
+#intersection(-1031 + 86, 4, 508)
+#vertical_street(-1031 + 108, 4, 508)
+#horizontal_street(-1031 + 193, 4, 508 + 78)
+#intersection(-1031 + 172, 4, 508)
 
-sidewalk = Structure.Structure('sidewalk_object_middle',-384, 4, 497, 3)
-sidewalk.place()
+#middle city network
+#intersection(-1031, 4, 508 + 86)
+#vertical_street(-1031 + 22, 4, 508 + 86)
+#intersection(-1031 + 86, 4, 508 + 86)
+#vertical_street(-1031 + 108, 4, 508+ 86)
+#intersection(-1031 + 172, 4, 508+ 86)
+
+#right side city network
+#intersection(-1031, 4, 508+172)
+#horizontal_street(-1031 + 21, 4, 508 + 78 + 86)
+#vertical_street(-1031 + 22, 4, 508+ 172)
+#horizontal_street(-1031 + 21 + 86, 4, 508 + 78 + 86)
+#intersection(-1031 + 86, 4, 508+172)
+#vertical_street(-1031 + 108, 4, 508+172)
+#horizontal_street(-1031 + 21 + 86 + 86, 4, 508 + 78 + 86)
+#intersection(-1031 + 172, 4, 508+172)
+
+#horizontal_street(-385, 4, 217)
+#sidewalk = Structure.Structure('street/sidewalk_object_middle',-384, 4, 497, 1)
+#sidewalk.place()
+
+#insert your x,y,z values y should be 4!
+city_network(-380, 4, 14)
