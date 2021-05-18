21bdcc53b4566b4dc5232c58eac206f8f197bbdb
Niels NTG <ntg999@gmail.com>
2021-05-18 23:25:17 +0200

    Fix structure origin and far corner calculation

--- House.py
+++ House.py
@@ -1,4 +1,5 @@
 import WorldEdit
+import mapUtils
 from Structure import Structure
 import numpy as np
 import os
@@ -47,20 +48,33 @@ class House:
         for floorIndex in range(self.numberOfFloors):
 
             if floorIndex == 0:
-                structure = self._getStructure('bottomFloor')
+                structure = self._getStructure('bottomFloor', yOffset)
             elif floorIndex == self.numberOfFloors - 1:
-                structure = self._getStructure('roof')
+                structure = self._getStructure('roof', yOffset)
             else:
-                structure = self._getStructure('floor')
+                structure = self._getStructure('floor', yOffset)
 
-            self._buildStructure(structure, yOffset)
+            self._buildStructure(structure)
+
+            structureOrigin = structure.getOriginInWorldSpace()
+            structureFarCorner = structure.getFarCornerInWorldSpace()
+            structureFarCorner[1] = structure.y
+            floorPerimiter = [
+                *structureOrigin,
+                *structureFarCorner
+            ]
+            print(floorPerimiter)
+            # TODO draw floor by projecting point outwards? https://stackoverflow.com/questions/9605556/how-to-project-a-point-onto-a-plane-in-3d
 
             yOffset = yOffset + structure.getSizeY()
 
-    def _getStructure(self, structureType):
-        return Structure(self.rng.choice(self.structures[structureType]), rotation=self.rotation)
+    def _getStructure(self, structureType, yOffset):
+        return Structure(
+            self.rng.choice(self.structures[structureType]),
+            x=self.x, y=self.y + yOffset, z=self.z,
+            rotation=self.rotation
+        )
 
-    def _buildStructure(self, structure, yOffset):
-        structure.setPosition(self.x, self.y + yOffset, self.z)
+    def _buildStructure(self, structure):
         structure.place()
 
